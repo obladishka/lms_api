e7a1cbd778a05f80c1e8c8f48ea80c17b01da93d
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-01-12 13:34:44 +0300

    add CRUD for lessons

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,21 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from courses.apps import CoursesConfig
-from courses.views import CourseViewSet
+from courses.views import (CourseViewSet, LessonCreateAPIView, LessonDestroyAPIView, LessonListAPIView,
+                           LessonRetrieveAPIView, LessonUpdateAPIView)
 
 app_name = CoursesConfig.name
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="course")
-urlpatterns = [] + router.urls
+
+urlpatterns = [
+    path("lessons/new", LessonCreateAPIView.as_view(), name="create_lesson"),
+    path("lessons", LessonListAPIView.as_view(), name="lesson_list"),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
+    path("lessons/<int:pk>/update", LessonUpdateAPIView.as_view(), name="update_lesson"),
+    path("lessons/<int:pk>/delete", LessonDestroyAPIView.as_view(), name="delete_lesson"),
+]
+
+urlpatterns += router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,32 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 
-from courses.models import Course
-from courses.serializers import CourseSerializer
+from courses.models import Course, Lesson
+from courses.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
