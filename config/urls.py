0175d5fa83d9879901797d086c1cc0d8542547a6
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-01-12 13:54:27 +0300

    add generics for creating and updating users

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("courses.urls", namespace="courses")),
+    path("", include("users.urls", namespace="users")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
