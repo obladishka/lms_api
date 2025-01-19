dffd945e4b1422441077b805fa410305b6eb9112
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-01-19 17:25:17 +0300

    add lessons detail info to course serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
     amount_of_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(read_only=True, many=True)
 
     def get_amount_of_lessons(self, course):
         """Calculating amount of lessons of a course."""
@@ -12,10 +19,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ("id", "name", "description", "amount_of_lessons")
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+        fields = ("id", "name", "description", "amount_of_lessons", "lessons")
