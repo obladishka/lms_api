c575dd998608f14116c20a3a9aa9875833f4f368
Goldyban Nastya <nasty-goldyba@ya.ru>
2025-01-25 21:35:40 +0300

    add crud for users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -8,6 +9,13 @@ from users.serializers import PaymentSerializer, UserSerializer
 
 class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.is_active = True
+        user.save()
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
@@ -18,6 +26,15 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
 
 
 class PaymentListAPIView(generics.ListAPIView):
